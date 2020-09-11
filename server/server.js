9b2640beb0e4c14920ad52a401afee758267362a
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-11 18:56:06 -0500

    sourced in all files

--- server/server.js
+++ server/server.js
@@ -9,17 +9,17 @@ app.use( express.static( 'server/public' ) );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 
 // globals
-const post = 3000;
-const Pool = pg.Pool;
+const port = 3000;
+/*const Pool = pg.Pool;
 const pool = new Pool( {
     database: 'weekend-to-do-app',
     host: 'localhost',
     post: 5432,
     max: 12,
     idleTimeoutMillis: 30000
-} );
+} );*/
 
 // listen
-app.listen( post, () => {
+app.listen( port, () => {
     console.log( 'listen on port', port );
 } );
